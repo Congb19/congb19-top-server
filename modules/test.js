1c2b93215bc4f0454a23fea7b270b5a8fdea25c5
linyingcong <linyingcong@edusoho.com>
2020-09-28 17:44:33 +0800

    9.28 db配置，mvc模式搭建

---
+++ modules/test.js
@@ -0,0 +1,44 @@
+/**
+├── modules
+    └── test.js
+*/
+
+// 引入刚刚在第五点建立连接mysql数据库的db.js文件
+const db = require("../config/db");
+// 引入Sequelize对象
+const Sequelize = db.sequelize;
+// 引入上一步的文章数据表模型文件
+const Test = Sequelize.import("../schema/test");
+// 自动创建表
+Test.sync({ force: false });
+
+class TestModel {
+	/**
+	 * 创建文章模型
+	 * @param data
+	 * @returns {Promise<*>}
+	 */
+	static async createTest(data) {
+		return await Test.create({
+			title: data.title, // 文章标题
+			author: data.author, // 文章作者
+			content: data.content, // 文章内容
+			category: data.category, // 文章分类
+		});
+	}
+
+	/**
+	 * 查询取文章详情数据
+	 * @param id  文章ID
+	 * @returns {Promise<Model>}
+	 */
+	static async getTestDetail(id) {
+		return await Test.findOne({
+			where: {
+				id,
+			},
+		});
+	}
+}
+
+module.exports = TestModel;
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,24 @@
-const router = require('koa-router')()
+const Router = require("koa-router");
+// const TestController = require('../controllers/test')
 
-router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
+const router = new Router({
+	prefix: "/api",
+});
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
+router.get("/", async (ctx, next) => {
+	await ctx.render("index", {
+		title: "Hello Koa 2!",
+	});
+});
 
-router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
-  }
-})
+router.get("/string", async (ctx, next) => {
+	ctx.body = "koa2 string";
+});
 
-module.exports = router
+router.get("/json", async (ctx, next) => {
+	ctx.body = {
+		title: "koa2 json",
+	};
+});
+
+module.exports = router;
