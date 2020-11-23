9be1d176a736f86192f848775ed6c3e0d3e5d469
Congb19 <347006087@qq.com>
2020-11-23 17:45:52 +0800

    11.23 simple signup

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ congb19-top 网站后端。
 ## 技术栈
 
 koa2, mysql
+
+### 开发环境
+
+example:
+
+http://localhost:8001/api/public/getDate
+
+`yarn dev`
+
+### 服务器运行
+
+`npm run prd`
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const router = require("koa-router")();
 
-router.prefix("/users");
+router.prefix("/api/users");
 
 router.get("/", function (ctx, next) {
 	ctx.body = "this is a users response!";
@@ -8,6 +8,19 @@ router.get("/", function (ctx, next) {
 
 router.get("/bar", function (ctx, next) {
 	ctx.body = "this is a users/bar response";
+});
+
+router.post("/signup", function (ctx, next) {
+	const req = ctx.request.body;
+	console.log(req);
+	let res = `signup ok`;
+	ctx.body = {
+		title: `signup`,
+		code: 200,
+		data: {
+			res,
+		},
+	};
 });
 
 module.exports = router;
