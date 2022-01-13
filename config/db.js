dc8dd3905fcf9974dcaff9ebb62b74b73f381a63
Congb19 <347006087@qq.com>
2022-01-13 10:58:03 +0800

    2022.1.13

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize("test", "test", "QWER1234", {
-	host: "localhost",
+	host: "159.75.85.197",
 	dialect: "mysql",
 	// operatorsAliases: false,
 	dialectOptions: {
